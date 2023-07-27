65bf920ebc5053a91252199c640970f338eeb158
Dae <65160408+phumthep@users.noreply.github.com>
2023-07-27 13:02:51 +0800

    Update scratch.py

--- src/scratch.py
+++ src/scratch.py
@@ -1,4 +1,4 @@
-
+#aaaa
 
 unit_g = 'pGas'
 
@@ -10,20 +10,26 @@ for con in CONS:
     print(model.getRow(con), con.sense, con.rhs)
 
 
-def c_trajec_up_bound_short(unit_g):
-    for unit_g in thermal_units:
-        time_RU = floor((max_cap[unit_g] - SU[unit_g])/RU[unit_g])
-        min_val = min(TU[unit_g]-1, time_RU)
-        
-        model.addConstrs(
-         (
-             pbar[unit_g, t]
-             <= (max_cap[unit_g] - min_cap[unit_g])*u[unit_g, t]
-                 - gp.quicksum(
-                     max_cap[unit_g] - SU[unit_g] - i*RU[unit_g]*v[unit_g, t-i]
-                     for i in range(0, min_val+1)
-                     )
-            for t in timesteps
-                 ),
-         name = 'trajecUpBoundShort' + f'_{unit_g}'
-         )
+set_objective()
+c_link_p()
+c_link_unit_status()
+# c_min_up_init()
+# c_min_down_init()
+# c_min_up()
+# c_min_down()
+c_p_bound()
+# c_switch_ramp_bound()
+# c_peak_up_bound()
+# c_peak_down_bound()
+# c_trajec_up_bound()
+# c_trajec_down_bound()
+# c_ramp_up()
+# c_ramp_down()
+# c_angle_dff()
+# c_max_flow()
+# c_ref_node()
+c_flow_balance()
+# c_reserve_req()
+c_renewables_bound()
+
+model.optimize()
