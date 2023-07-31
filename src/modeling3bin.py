424a5b124e82cdcfd3da5bfbebfecdba0842dbdb
Dae <65160408+phumthep@users.noreply.github.com>
2023-07-31 20:56:51 +0800

    Update modeling3bin.py

--- src/modeling3bin.py
+++ src/modeling3bin.py
@@ -25,8 +25,10 @@ Style guide:
 '''
 
 
-HOURLY_TRANSMISSION_AVAI = False
+T = 24
+TIMESTEPS = range(1, T+1)
 
+HOURLY_TRANSMISSION_AVAI = False
 
 DATE_COLS = ['year', 'month', 'day', 'hour']
 
@@ -36,9 +38,6 @@ model = gp.Model('tight_formulation')
 
 
 #---- Section: System parameters
-
-T = 24
-timesteps = range(1, T+1) # Be careful with Python indexing!
 
 thermal_units = pd.read_csv(
     '..\\data\\user_inputs\\generators.csv', 
@@ -166,7 +165,7 @@ max_linecap = linecap.max().max()
 p = model.addVars(thermal_units, range(1,T+1), vtype=GRB.CONTINUOUS, lb=0, name='p')
 
 # The maximum power available above minimum capacity is in MW
-pbar = model.addVars(thermal_units, timesteps, vtype=GRB.CONTINUOUS, lb=0, name='pbar')
+pbar = model.addVars(thermal_units, TIMESTEPS, vtype=GRB.CONTINUOUS, lb=0, name='pbar')
 
 # Separate the renewable dispatch variable
 prnw = model.addVars(
@@ -175,10 +174,10 @@ prnw = model.addVars(
 # p2 = model.addVars(thermal_units, range(1,T+1), vtype=GRB.CONTINUOUS, lb=0, name='p2')
 
 # Spinning reserve is in MW
-spin = model.addVars(thermal_units, timesteps, vtype=GRB.CONTINUOUS, lb=0, name='spin')
+spin = model.addVars(thermal_units, TIMESTEPS, vtype=GRB.CONTINUOUS, lb=0, name='spin')
 
 # System reserve is in MW
-rsys = model.addVars(timesteps, vtype=GRB.CONTINUOUS, lb=0, name='rsys')
+rsys = model.addVars(TIMESTEPS, vtype=GRB.CONTINUOUS, lb=0, name='rsys')
 
 # The flow variable f(a,b,t) represents the flow in the
 # line segment (a,b) at hour t in MW/hr).
@@ -187,7 +186,7 @@ rsys = model.addVars(timesteps, vtype=GRB.CONTINUOUS, lb=0, name='rsys')
 # flowing from b to a.
 # We set the bounds based on the largest transmission line.
 flow = model.addVars(
-    arcs, timesteps,
+    arcs, TIMESTEPS,
     lb = -max_linecap,
     ub = max_linecap,
     vtype = GRB.CONTINUOUS,
@@ -195,27 +194,27 @@ flow = model.addVars(
     )
 
 # We adopt the 2-bin formulation with unit status and switch-on variables
-u = model.addVars(thermal_units, timesteps, vtype=GRB.BINARY, name='status')
-v = model.addVars(thermal_units, timesteps, vtype=GRB.BINARY, name='on')
-w = model.addVars(thermal_units, timesteps, vtype=GRB.BINARY, name='shut')
+u = model.addVars(thermal_units, TIMESTEPS, vtype=GRB.BINARY, name='status')
+v = model.addVars(thermal_units, TIMESTEPS, vtype=GRB.BINARY, name='on')
+w = model.addVars(thermal_units, TIMESTEPS, vtype=GRB.BINARY, name='shut')
 
 # The volt angles are in radians
 # theta = model.addVars(
-# nodes, timesteps, vtype=GRB.CONTINUOUS, lb=-pi, ub=pi, name='volt_angle')
+# nodes, TIMESTEPS, vtype=GRB.CONTINUOUS, lb=-pi, ub=pi, name='volt_angle')
 
 theta = model.addVars(
-    nodes, timesteps, lb=-2000000000, vtype=GRB.CONTINUOUS, name='volt_angle'
+    nodes, TIMESTEPS, lb=-2000000000, vtype=GRB.CONTINUOUS, name='volt_angle'
     )
 
 # Load mismatch variables
 s_pos = model.addVars(
-    nodes, timesteps, vtype=GRB.CONTINUOUS, lb = 0, name='s_pos')
+    nodes, TIMESTEPS, vtype=GRB.CONTINUOUS, lb = 0, name='s_pos')
 
 s_neg = model.addVars(
-    nodes, timesteps, vtype=GRB.CONTINUOUS, lb = 0, name='s_neg')
+    nodes, TIMESTEPS, vtype=GRB.CONTINUOUS, lb = 0, name='s_neg')
 
 # System wide excess. I don't think we need this
-# sys_shortfall = model.addVars(timesteps, vtype=GRB.CONTINUOUS, lb = 0, name='load_under')
+# sys_shortfall = model.addVars(TIMESTEPS, vtype=GRB.CONTINUOUS, lb = 0, name='load_under')
 
 model.update()
 
@@ -278,7 +277,7 @@ def c_get_p():
     model.addConstrs(
         (
             p2[unit_g, t] == p[unit_g, t] + min_cap[unit_g] * u[unit_g, t]
-            for unit_g in thermal_units for t in timesteps
+            for unit_g in thermal_units for t in TIMESTEPS
             ),
         name = 'get_p'
         )
@@ -288,7 +287,7 @@ def c_link_pu():
     model.addConstrs(
         (
             p2[unit_g, t] <= max_cap[unit_g] * u[unit_g, t]
-            for unit_g in thermal_units for t in timesteps
+            for unit_g in thermal_units for t in TIMESTEPS
             ),
         name = 'link_pu'
         )
@@ -299,7 +298,7 @@ def c_link_p():
     model.addConstrs(
         (
             pbar[unit_g, t] == p[unit_g, t] + spin[unit_g, t]
-            for unit_g in thermal_units for t in timesteps
+            for unit_g in thermal_units for t in TIMESTEPS
             ),
         name = 'link_p'
         )
@@ -316,7 +315,7 @@ def c_link_unit_status():
             ),
         name = 'link_uv_init'
         )
-    # For timesteps t in [2, 24]
+    # For TIMESTEPS t in [2, 24]
     model.addConstrs(
         (
             u[unit_g, t] - u[unit_g, t-1] 
@@ -381,7 +380,7 @@ def c_p_bound():
     model.addConstrs(
         (
             p[unit_g, t] <= pbar[unit_g, t]
-            for t in timesteps for unit_g in thermal_units
+            for t in TIMESTEPS for unit_g in thermal_units
             ),
         name = 'upper_p'
         )
@@ -467,7 +466,7 @@ def c_trajec_up_bound():
             # Note TU_g - 1, which is different from the above
             min_val = min(TU[unit_g]-1, time_RU)
             
-            for t in timesteps:
+            for t in TIMESTEPS:
                 # Define the summation term
                 sum_term = 0
                 for i in range(0, min_val+1):
@@ -499,7 +498,7 @@ def c_trajec_down_bound():
         time_RU = floor((max_cap[unit_g] - SU[unit_g])/RU[unit_g])
         time_RD = floor((max_cap[unit_g] - SU[unit_g])/RD[unit_g])
         
-        for t in timesteps:
+        for t in TIMESTEPS:
             KSD_t = min(time_RD, TU[unit_g]-1, T-t-1)
             
             # Omit adding inequalities if KSD < 0 because
@@ -575,7 +574,7 @@ def c_ramp_down():
         name = 'rampDownInit'
         )
     
-    # Ramping at other timesteps
+    # Ramping at other TIMESTEPS
     model.addConstrs(
         (
             p[unit_g, t-1] - p[unit_g, t]
@@ -593,14 +592,14 @@ def c_flow_bound():
     model.addConstrs(
         (
             flow[a, b, t] <= linecap[(a, b)]
-            for (a, b) in arcs for t in timesteps
+            for (a, b) in arcs for t in TIMESTEPS
             ),
         name = 'maxFlow'
         )
     model.addConstrs(
         (
             flow[a, b, t] >= -linecap[(a, b)]
-            for (a, b) in arcs for t in timesteps
+            for (a, b) in arcs for t in TIMESTEPS
             ),
         name = 'minFlow'
         )
@@ -610,7 +609,7 @@ def c_angle_dff():
     model.addConstrs(
         (
             flow[a, b, t] == suscept.loc[t, (a, b)] * (theta[a, t] - theta[b, t])
-            for (a, b) in arcs for t in timesteps
+            for (a, b) in arcs for t in TIMESTEPS
         ),
         name = 'angleDiffForward'
         )
@@ -618,7 +617,7 @@ def c_angle_dff():
     model.addConstrs(
         (
             flow[a, b, t] == suscept.loc[t, (a, b)] * (theta[a, t] - theta[b, t])
-            for (a, b) in arcs for t in timesteps
+            for (a, b) in arcs for t in TIMESTEPS
         ),
         name = 'angleDiffBack'
         )
@@ -626,7 +625,7 @@ def c_angle_dff():
 
 def c_ref_node():
     model.addConstrs(
-        (theta[max_node, t] == 0 for t in timesteps), 
+        (theta[max_node, t] == 0 for t in TIMESTEPS), 
         name='refNode'
         )
 
@@ -636,7 +635,7 @@ def c_flow_balance():
     # this allows flow_ab + flow_ba = 0 with non-zero flows
     # In reality, if the demand is zero, then there should be no flow
     # Must link flow to generation...
-    for t in timesteps:
+    for t in TIMESTEPS:
         for (a, b) in arcs:
             # If n is a thermal unit, then it can generate energy
             if a in thermal_units:
@@ -678,7 +677,7 @@ def c_reserve_req():
             gp.quicksum(pbar[unit_g, t] for unit_g in thermal_units)
                 + rsys[t]
             >= gp.quicksum(demand.loc[t, n] for n in nodes_w_demand) + spin_req[t]
-            for t in timesteps
+            for t in TIMESTEPS
             ),
         name = 'reserveReq'
         )
@@ -688,7 +687,7 @@ def c_renewables_bound():
     model.addConstrs(
         (
             prnw[unit_w, t] <= re_cap.loc[t, unit_w]
-            for t in timesteps for unit_w in re_units
+            for t in TIMESTEPS for unit_w in re_units
             ),
         name = 'renewLimit'
         )
