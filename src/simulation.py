40eb78763dd606e9ef034546a075ce74989cba16
Dae <65160408+phumthep@users.noreply.github.com>
2023-08-03 12:32:02 +0800

    Create simulation.py
    This file contains the Simulator class.

---
+++ src/simulation.py
@@ -0,0 +1,54 @@
+from math import floor
+import pandas as pd
+
+from core.builder import ModelBuilder
+from processing.functions import create_init_condition
+from processing.input import SystemInput
+from processing.record import SystemRecord
+
+
+# TODO: Implement warm start
+
+
+class Simulator:
+    def __init__(self, T: int, model_folder: str) -> None:
+        self.T = T
+        self.model_folder = model_folder
+        
+        
+    def run(self) -> None:
+        # Instantiate objects
+        system_record = SystemRecord()
+        
+        system_input = SystemInput(model_folder=self.model_folder, T=self.T)
+        builder = ModelBuilder(system_input)
+        
+        # One year has 8760 hours. If T = 24, then we have 365 steps.
+        steps = floor(8760/self.T)
+        
+        # Initially, we can define the initial conditions
+        init_conds = create_init_condition(system_input.thermal_units)
+        
+        # The indexing of 'i' starts at zero because we use this to
+        # index the parameters of future simulation periods (t + self.i*self.T)
+        for k in range(0, steps):
+            # Create a gurobipy model for each simulation period
+            model = builder.build(
+                k = k,
+                init_conds = init_conds)
+            
+            model.optimize()
+            
+            system_record.keep(model)
+            init_conds = system_record.get_init_conds()
+        
+        # Export the final results somewhere
+        system_record.to_csv()
+        return system_record.get_record()
+    
+    
+    
+    
+if __name__ == '__main__':
+    simulator = Simulator(T=24, model_folder='user_inputs')
+    simulator.run()
