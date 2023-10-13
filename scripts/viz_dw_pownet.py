51bc5bc88e09784892ae4a5bb2f5bc06e8ff4af8
Dae <65160408+phumthep@users.noreply.github.com>
2023-10-13 16:27:10 -0400

    Visualize DW statistics across simulation horizon

---
+++ scripts/viz_dw_pownet.py
@@ -0,0 +1,46 @@
+import os
+
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+PDIR = os.path.dirname(os.getcwd())
+TEMPDIR = os.path.join(PDIR, 'temp')
+FILENAME = '20231013_1602_laos_dw_stats'
+
+
+
+#=================== Plotting
+
+dw_times = pd.read_csv(
+    os.path.join(TEMPDIR, f'{FILENAME}.csv'), 
+    header = 0, 
+    usecols = ['master_time', 'subp_time'])
+
+dw_itercounts = pd.read_csv(
+    f'..//temp//{FILENAME}.csv', 
+    header = 0, 
+    usecols = ['master_iter', 'subp_iter'])
+
+# Plot the time
+fig, ax = plt.subplots()
+dw_times.plot.bar(stacked=True, ax=ax)
+ax.set(xlabel='Day', ylabel='Time (s)')
+plt.savefig(
+    os.path.join(TEMPDIR, f'{FILENAME}_time.png'),
+    dpi = 350)
+plt.show()
+
+
+# Plt the itercount
+fig, ax = plt.subplots()
+dw_itercounts.plot.bar(stacked=True, ax=ax)
+ax.set(xlabel='Day', ylabel='Itercount')
+plt.savefig(
+    os.path.join(TEMPDIR, f'{FILENAME}_itercount.png'),
+    dpi = 350)
+plt.show()
+
+
+
+
