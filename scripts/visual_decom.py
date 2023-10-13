904cb0b2a65efa20f8f48004ce0c5bd8719a02fe
Dae <65160408+phumthep@users.noreply.github.com>
2023-10-13 09:38:39 -0400

    Update visual_decom.py

--- scripts/visual_decom.py
+++ scripts/visual_decom.py
@@ -1,15 +1,14 @@
 import os
 
-import pandas as pd
 import matplotlib.pyplot as plt
 import mplcursors
 
-from pydopt.preprocessing.functions import parse_instance
+from pypolp.tools.parser import parse_mps_dec
 
 
 
 
-MODEL_NAME = 'power_system'
+MODEL_NAME = 'cambodia_by_units'
 
 # Get the A matrix as a dataframe
 
@@ -17,14 +16,18 @@ pdir = os.path.dirname(os.getcwd())
 path_mps = os.path.join(pdir, 'temp', 'decom_files', f'{MODEL_NAME}.mps')
 path_dec = os.path.join(pdir, 'temp', 'decom_files', f'{MODEL_NAME}.dec')
     
+dw_problem = parse_mps_dec(path_mps, path_dec)
+
 (
- c_df, A_df, b_df, ineq_df,
- col_df, subp_indices, col_indices
- ) = parse_instance(path_mps, path_dec)
+ c_df, A_df, b_df, ineq_df, col_df
+ ) = dw_problem.get_dataframes()
+
+subp_indices = dw_problem.row_indices
+col_indices = dw_problem.col_indices
 
 # Sort the constraints in order according to the .DEC file
 fig, ax = plt.subplots(figsize=(4,7))
-ax.spy(A_df, markersize=5)
+ax.spy(A_df, markersize=1)
 ax.set_xlabel('Variable ID')
 ax.set_ylabel('Constraint ID')
 
@@ -37,5 +40,7 @@ def on_add(sel):
     tt = constr_name + '\n' + var_name
     sel.annotation.get_bbox_patch().set(fc="white")
     sel.annotation.set(text=tt, fontsize=13, ha='left')
-    
+
+plt.savefig(
+    os.path.join(pdir, 'temp', f'{MODEL_NAME}_structure.png'), dpi=350)
 plt.show()
