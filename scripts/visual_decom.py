cca9011266e9bb4aa80148dc8db08e4548474f6c
Dae <65160408+phumthep@users.noreply.github.com>
2023-10-16 13:35:53 -0400

    Update visual_decom.py
    Fixed using float to index dataframes.

--- scripts/visual_decom.py
+++ scripts/visual_decom.py
@@ -8,7 +8,7 @@ from pypolp.tools.parser import parse_mps_dec
 
 
 
-MODEL_NAME = 'cambodia_by_units'
+MODEL_NAME = 'laos_by_units'
 
 # Get the A matrix as a dataframe
 
@@ -26,7 +26,7 @@ subp_indices = dw_problem.row_indices
 col_indices = dw_problem.col_indices
 
 # Sort the constraints in order according to the .DEC file
-fig, ax = plt.subplots(figsize=(4,7))
+fig, ax = plt.subplots(figsize=(7,7))
 ax.spy(A_df, markersize=1)
 ax.set_xlabel('Variable ID')
 ax.set_ylabel('Constraint ID')
@@ -35,12 +35,12 @@ ax.set_ylabel('Constraint ID')
 cursor = mplcursors.cursor(ax, hover=True)
 @cursor.connect("add")
 def on_add(sel):
-    constr_name = 'Constraint: ' + str(A_df.index[sel.target[1]])
-    var_name = 'Variable: ' + str(A_df.columns[sel.target[0]])
+    constr_name = 'Constraint: ' + str(A_df.index[int(sel.target[1])])
+    var_name = 'Variable: ' + str(A_df.columns[int(sel.target[0])])
     tt = constr_name + '\n' + var_name
     sel.annotation.get_bbox_patch().set(fc="white")
     sel.annotation.set(text=tt, fontsize=13, ha='left')
 
 plt.savefig(
-    os.path.join(pdir, 'temp', f'{MODEL_NAME}_structure.png'), dpi=350)
+    os.path.join(pdir, 'temp', f'{MODEL_NAME}_structure.png'), dpi=350, bbox_inches='tight')
 plt.show()
