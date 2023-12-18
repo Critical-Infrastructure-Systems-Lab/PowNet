70aab46735194ec868c804619f7ee4bb0bc8efbf
Dae <65160408+phumthep@users.noreply.github.com>
2023-12-18 15:20:05 -0500

    Update viz_rounding_stats.py

--- scripts/viz_rounding_stats.py
+++ scripts/viz_rounding_stats.py
@@ -14,7 +14,7 @@ from pownet.folder_sys import get_temp_dir, get_output_dir
 from functions import calc_percent_change
 
 
-MODEL_NAME = 'laos'
+MODEL_NAME = 'cambodia'
 
 
 
@@ -56,7 +56,7 @@ for file in files:
 #%% Analyze feasibility metric
 # Feasibility metric: the number of times the solution is feasible and is integer
 valid_fraction = compiled_df.groupby(
-    ['model_name', 'direction', 'threshold', 'is_adaptive']
+    ['model_name', 'direction', 'threshold']
     )['is_valid'].mean()
 
 
@@ -74,7 +74,7 @@ plt.show()
 
 #%% Plot the MIP gap
 gap_df = compiled_df.groupby(
-    ['model_name', 'direction', 'threshold', 'is_adaptive']
+    ['model_name', 'direction', 'threshold']
 )['lp_mip_gap'].mean().reset_index()
 
 # Create a grid of subplots for each unique value in the 'direction' column
@@ -85,7 +85,7 @@ plt.show()
 
 #%% Plot the computational time
 speed_df = compiled_df.groupby(
-    ['model_name', 'direction', 'threshold', 'is_adaptive']
+    ['model_name', 'direction', 'threshold']
 )['x_speed_up'].mean().reset_index()
 
 # Create a grid of subplots for each unique value in the 'direction' column
@@ -98,18 +98,45 @@ plt.show()
 
 # Print the average speed up
 print('\nAverage speed up:')
-print(speed_df.groupby(['model_name', 'direction', 'is_adaptive'])['x_speed_up'].mean())
+print(speed_df.groupby(['model_name', 'direction'])['x_speed_up'].mean())
 
 # Print the average MIP gap
 print('\nAverage MIP gap:')
-print(gap_df.groupby(['model_name', 'direction', 'is_adaptive'])['lp_mip_gap'].mean())
+print(gap_df.groupby(['model_name', 'direction'])['lp_mip_gap'].mean())
 
 # Print the average feasibility
 print('\nAverage feasibility:')
-print(valid_fraction_df.groupby(['model_name', 'direction', 'is_adaptive'])['is_valid'].mean())
+print(valid_fraction_df.groupby(['model_name', 'direction'])['is_valid'].mean())
 
 
+#%% What causes infeasibility?
+
+infeasibility_df = compiled_df.loc[~compiled_df['is_valid']].copy()
+infeasibility_df['infeasibility_reason'] = None
+infeasibility_df.loc[~infeasibility_df['rounding_is_int'], 'infeasibility_reason'] = 'Not integer'
+infeasibility_df.loc[~infeasibility_df['iter_rounding_feasible'], 'infeasibility_reason'] = 'Not feasible'
+infeasibility_df.loc[~infeasibility_df['rounding_is_int'] & ~infeasibility_df['iter_rounding_feasible'], 'infeasibility_reason'] = 'Both'
+
+infeasibility_df = infeasibility_df.groupby(
+    ['model_name', 'direction', 'threshold', 'infeasibility_reason']
+)['infeasibility_reason'].count().reset_index(name='count'
+)
+pivot_df = infeasibility_df.pivot_table(index=['threshold', 'direction'], columns='infeasibility_reason', values='count', fill_value=0)
+
+melt_df = pivot_df.reset_index().melt(id_vars=['threshold', 'direction'], var_name='infeasibility_reason', value_name='count')
+
+# Create a FacetGrid
+g = sns.FacetGrid(melt_df, col='direction', height=6, aspect=1)
+
+# Map the barplot to each subplot
+g.map(sns.barplot, 'threshold', 'count', 'infeasibility_reason', palette='deep', ci=None)
+
+# Add a legend
+g.add_legend()
+
+# Show the plot
+plt.show()
 #%% Plot histogram of k
 compiled_df.groupby(
-    ['model_name', 'direction', 'is_adaptive']
+    ['model_name', 'direction']
     )['iter_rounding_k'].hist(legend=['both', 'up', 'adaptive'])
