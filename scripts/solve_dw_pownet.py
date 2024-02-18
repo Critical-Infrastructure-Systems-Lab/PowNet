bb4d0f860e53dc78736b35fb5c693df172fd6feb
Dae <65160408+phumthep@users.noreply.github.com>
2024-02-17 22:12:23 -0500

    Update solve_dw_pownet.py

--- scripts/solve_dw_pownet.py
+++ scripts/solve_dw_pownet.py
@@ -1,6 +1,6 @@
 """
 Run experments with Dantzig-Wolfe decomposition for the PowNet model
-The script will collect statistics for each day and save them in a csv file
+The script will collect statistics of each instance and save them in a csv file.
 """
 
 import csv
@@ -8,154 +8,222 @@ from datetime import datetime
 import os
 
 import gurobipy as gp
+import pandas as pd
 
 from pownet.folder_sys import get_temp_dir, get_output_dir, count_mps_files
 from pypolp.dw.dw import DantzigWolfe, DWRecord
 from pypolp.parser import parse_mps_with_orders, parse_mps, get_dataframe_orders
-from pypolp.functions import check_is_binary_from_df
+from pypolp.functions import get_non_binary_from_df
+from solve_new_rounding_pownet import calculate_set_mipgap
 
 
-#### Define parameters ####
-MODEL_NAME = "thailand"
-T = "72"
-RECOVER_INT_FROM_DW = True
-DWOPTGAP = 10  # 0.0001
-RELAX_SUBPROBLEMS = False
+def run_dw_experiment(
+    model_name: str,
+    T_simulate: int,
+    set_rmpgap: float,
+    set_dwimprove: float,
+    relax_subproblems: bool,
+) -> None:
+    # Start time
+    timer_script = datetime.now()
+    ctime = timer_script.strftime("%Y%m%d_%H%M")
 
-###########################
+    true_objvals = pd.read_csv(
+        os.path.join(
+            get_temp_dir(),
+            "true_values",
+            f"{model_name}_{T_simulate}.csv",
+        ),
+        usecols=["mip_objval"],
+        header=0,
+    )
 
-# Start time
-start_time_script = datetime.now()
-CTIME = start_time_script.strftime("%Y%m%d_%H%M")
+    print(f"\nDW-PowNet: ==== Begin collecting statistics for {model_name} ====")
 
-print(f"\nDW-PowNet: ==== Begin collecting statistics for {MODEL_NAME} ====")
-print(f"Relax subproblems: {RELAX_SUBPROBLEMS}")
+    # Define the session name and create a folder to save the outputs
+    output_dir = os.path.join(get_temp_dir(), "new_dw_stats")
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-# Define the session name and create a folder to save the outputs
-session_name = f"{CTIME}_{MODEL_NAME}_{T}_{DWOPTGAP}_{RELAX_SUBPROBLEMS}"
-session_name = os.path.join(get_temp_dir(), session_name)
-if not os.path.exists(session_name):
-    os.makedirs(session_name)
+    session_name = f"{ctime}_{model_name}_{T_simulate}_{set_rmpgap}_{set_dwimprove}_{relax_subproblems}"
 
-# Need to extract row/column orders to parse the DW structure
-instance_folder = os.path.join(get_output_dir(), f"{MODEL_NAME}_{T}_instances")
-path_dec = os.path.join(instance_folder, f"{MODEL_NAME}.dec")
-path_mps = os.path.join(instance_folder, f"{MODEL_NAME}_0.mps")
-(_, A_df, _, _, col_df) = parse_mps(path_mps)
-row_order, col_order = get_dataframe_orders(path_dec, A_df, col_df)
-del A_df
-del col_df
+    # Extract row/column orders to parse the DW structure
+    instance_folder = os.path.join(
+        get_output_dir(), f"{model_name}_{T_simulate}_instances"
+    )
+    path_dec = os.path.join(instance_folder, f"{model_name}.dec")
+    path_mps = os.path.join(instance_folder, f"{model_name}_0.mps")
+    (_, A_df, _, _, col_df) = parse_mps(path_mps)
+    row_order, col_order = get_dataframe_orders(path_dec, A_df, col_df)
+    del A_df
+    del col_df
 
-# Collect statistics to compare computational performance
-FIELDS = [
-    "master_itercount",
-    "master_time",
-    "master_mip_time",  # Time to optimize the last interation of master as integer
-    "subp_itercount",
-    "subp_time",
-    "dw_itercount",
-    "dw_time",
-    "dw_mip_time",
-    "dw_objval",
-    "dw_mip_objval",
-    "mip_gurobi_time",
-    "mip_objval",
-    "lp_gurobi_time",
-    "lp_objval",
-    "is_int",  # Check if the lp solution is integer
-    # Wall clock is the total time to build, solve, extract solution
-    "wall_clock_dw",
-    "wall_clock_mip_gurobi",
-    "wall_clock_lp_gurobi",
-]
+    # Collect statistics to compare computational performance
+    FIELDS = [
+        "model_name",
+        "T_simulate",
+        "set_rmpgap",
+        "set_dwimprove",
+        "master_itercount",
+        "master_time",
+        "master_mip_time",  # Time to reoptimize with binary weights
+        "subp_itercount",
+        "subp_time",
+        "dw_itercount",
+        "dw_total_time",
+        "dw_objval",
+        "dw_rmpgap",
+        "dw_improve",
+        "dw_mip_objval",
+        "set_mipgap",  # Competitive MIPGap for Gurobi
+        "mip_gurobi_time",
+        "mip_objval",
+        "lp_gurobi_time",
+        "lp_objval",
+        "wall_clock_dw",
+        "wall_clock_mip_gurobi",
+        "wall_clock_lp_gurobi",
+        "true_objval",
+    ]
 
-# Create a csv file with only headers. We will append to this csv later.
-csv_name = os.path.join(get_temp_dir(), f"{session_name}_dwstats.csv")
-with open(csv_name, "w", newline="", encoding="utf-8") as csvfile:
-    # creating a csv writer object
-    csvwriter = csv.writer(csvfile)
-    # writing the fields
-    csvwriter.writerow(FIELDS)
-
-# Count the number of files ending with .mps
-# Days are labeled from k = 0 to k = 364 (max)
-num_instances = count_mps_files(instance_folder)
-for k in range(num_instances):
-    print(f"\n\nDW-PowNet: === Solving step {k} ===")
-    path_mps = os.path.join(instance_folder, f"{MODEL_NAME}_{k}.mps")
-
-    # ----- Solve with Dantzig-Wolfe
-    wall_clock_dw = datetime.now()
-    dw_problem = parse_mps_with_orders(path_mps, row_order, col_order)
-    record = DWRecord()
-    record.fit(dw_problem)
-
-    dw_instance = DantzigWolfe(dw_optgap=DWOPTGAP, relax_subproblems=RELAX_SUBPROBLEMS)
-
-    dw_instance.fit(dw_problem, record)
-    dw_instance.solve(record)
-
-    master_time, subp_time = dw_instance.get_stats("runtime")
-    master_itercount, subp_itercount = dw_instance.get_stats("itercount")
-
-    dw_objval, dw_solution = dw_instance.get_solution(record)
-    dw_itercount = dw_instance.dw_iter
-
-    wall_clock_dw = (datetime.now() - wall_clock_dw).total_seconds()
-    dw_time = master_time + subp_time
-
-    # ----- Solve with Dantzig-Wolfe but recover integer solution
-    if RECOVER_INT_FROM_DW:
-        dw_mip_objval, dw_solution_mip = dw_instance.get_solution(
-            record, recover_integer=True
-        )
-        master_mip_time = dw_instance.master_problem.model.runtime
-        dw_mip_time = dw_time + master_mip_time
-
-        # Check if the solution is integer
-        dw_solution_mip = dw_solution_mip.reset_index(names="name")
-        dw_is_int, dw_non_binary_vars = check_is_binary_from_df(
-            df=dw_solution_mip,
-            target_varnames=["status", "start", "shut"],
-            return_non_binary=False,
-        )
-
-        # Verify subproblems produce integer solutions
-        if not dw_is_int:
-            fname = f"dw_nonbin_{MODEL_NAME}_{k}.csv"
-            print(f"DW-PowNet: Saving {fname}...")
-            dw_non_binary_vars.to_csv(os.path.join(session_name, fname), index=False)
-            raise ValueError
-
-    else:
-        dw_mip_objval = None
-        dw_solution_mip = None
-        dw_mip_time = None
-
-    # ----- Saving intermediate results
-    with open(csv_name, "a", newline="", encoding="utf-8") as csvfile:
+    # Create a csv file with only headers. We will append to this csv later.
+    csv_name = os.path.join(output_dir, f"{session_name}_dwstats.csv")
+    with open(csv_name, "w", newline="", encoding="utf-8") as csvfile:
         # creating a csv writer object
         csvwriter = csv.writer(csvfile)
-        # writing the data rows
-        csvwriter.writerow(
-            [
-                master_itercount,
-                master_time,
-                master_mip_time,
-                subp_itercount,
-                subp_time,
-                dw_itercount,
-                dw_time,
-                dw_mip_time,
-                dw_objval,
-                dw_mip_objval,
-                wall_clock_dw,
-            ]
+        # writing the fields
+        csvwriter.writerow(FIELDS)
+
+    # Count the number of files ending with .mps
+    # Days are labeled from k = 0 to k = 364 (max)
+    num_instances = count_mps_files(instance_folder)
+    for k in range(5): #num_instances
+        print(f"\n\nDW-PowNet: === Solving step {k} ===")
+        path_mps = os.path.join(instance_folder, f"{model_name}_{k}.mps")
+
+        # ----- Solve with Dantzig-Wolfe
+        timer_dw = datetime.now()
+        dw_problem = parse_mps_with_orders(path_mps, row_order, col_order)
+        record = DWRecord()
+        record.fit(dw_problem)
+
+        dw_model = DantzigWolfe(
+            dw_rmpgap=set_rmpgap,
+            dw_improve=set_dwimprove,
+            relax_subproblems=relax_subproblems,
         )
 
-# Save solutions for future reference. Place them in a folder
-print(
-    f"\n\nDW-PowNet: ==== Completed collecting compute statistics for {MODEL_NAME} ===="
-)
-print(f"Results for {session_name}")
-print(f'{"Total time to complete:":<20} {datetime.now()- start_time_script}')
+        dw_model.fit(dw_problem, record)
+        dw_model.solve(record)
+
+        master_time, subp_time = dw_model.get_stats("runtime")
+        master_itercount, subp_itercount = dw_model.get_stats("itercount")
+        dw_rmpgap = dw_model.rmpgap
+        dw_improve = dw_model.incre_improve
+
+        timer_dw = (datetime.now() - timer_dw).total_seconds()
+
+        dw_objval, _ = dw_model.get_solution(record)
+        dw_itercount = dw_model.dw_iter
+
+        # ----- Reoptimize DW to recover integer solution
+        dw_model.reoptimize_with_binary_weights()
+        dw_mip_objval, dw_solution_mip = dw_model.get_solution(record)
+        master_mip_time = dw_model.master_problem.model.runtime
+
+        #  Verify subproblems produce integer solutions
+        dw_solution_mip = dw_solution_mip.reset_index(names="name")
+        non_bin_variables = get_non_binary_from_df(
+            df=dw_solution_mip,
+            target_varnames=["status", "start", "shut"],
+            )
+        dw_is_int = (len(non_bin_variables) == 0)
+        if not dw_is_int:
+            fname = f"dw_nonbin_{model_name}_{k}.csv"
+            print(f"DW-PowNet: Saving {fname}...")
+            non_bin_variables.to_csv(os.path.join(output_dir, fname), index=False)
+            raise ValueError("DW-PowNet: Subproblems do not produce integer solutions.")
+
+        # ----- Solve as MIP with Gurobi
+        # Set the MIPGap competitively to the rounding objective value
+        # Assume k represents the same instance
+        true_objval = true_objvals.loc[k, "mip_objval"]
+        set_mipgap = calculate_set_mipgap(dw_mip_objval, true_objval)
+        # Compare to Gurobi getting less than 50% of the optimal solution
+        if set_mipgap > 1:
+            set_mipgap = 0.50
+
+        timer_mip = datetime.now()
+
+        mip_model = gp.read(path_mps)
+        mip_model.optimize()
+        mip_objval = mip_model.objval
+        mip_gurobi_time = mip_model.runtime
+
+        timer_mip = (datetime.now() - timer_mip).total_seconds()
+
+        # ----- Solve as LP with Gurobi
+        timer_lp = datetime.now()
+
+        lp_model = mip_model.relax()
+        lp_model.optimize()
+        lp_objval = lp_model.objval
+        lp_gurobi_time = lp_model.runtime
+
+        timer_lp = (datetime.now() - timer_lp).total_seconds()
+
+        # ----- Saving intermediate results
+        with open(csv_name, "a", newline="", encoding="utf-8") as csvfile:
+            # creating a csv writer object
+            csvwriter = csv.writer(csvfile)
+            # writing the data rows
+            csvwriter.writerow(
+                [
+                    model_name,
+                    T_simulate,
+                    set_rmpgap,
+                    set_dwimprove,
+                    master_itercount,
+                    master_time,
+                    master_mip_time,
+                    subp_itercount,
+                    subp_time,
+                    dw_itercount,
+                    master_time + subp_time + master_mip_time, # dw_total_time
+                    dw_objval,
+                    dw_rmpgap,
+                    dw_improve,
+                    dw_mip_objval,
+                    set_mipgap,
+                    mip_gurobi_time,
+                    mip_objval,
+                    lp_gurobi_time,
+                    lp_objval,
+                    timer_dw,
+                    timer_mip,
+                    timer_lp,
+                    true_objval
+                ]
+            )
+
+    # Save solutions for future reference. Place them in a folder
+    print(f"\n\nDW-PowNet: ==== Completed {session_name} ====")
+    print(f'{"Total time to complete:":<20} {datetime.now()- timer_script}')
+
+
+if __name__ == "__main__":
+    #### Define parameters ####
+    model_name = "laos"
+    T_simulate = "24"
+    rmpgap = 10  # in percent
+    dw_improve = 1.0  # in percent
+
+    relax_subproblem = False
+
+    run_dw_experiment(
+        model_name=model_name,
+        T_simulate=T_simulate,
+        set_rmpgap=rmpgap,
+        set_dwimprove=dw_improve,
+        relax_subproblems=relax_subproblem,
+    )
