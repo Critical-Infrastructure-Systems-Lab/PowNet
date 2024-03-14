1ce60e249d20fa1875c2d4714441a5202c02ced4
Dae <65160408+phumthep@users.noreply.github.com>
2024-03-14 15:47:41 -0400

    Update viz_dw_pownet.py

--- scripts/viz_dw_pownet.py
+++ scripts/viz_dw_pownet.py
@@ -5,6 +5,7 @@ Dantzig-Wolfe always generate a feasible solution in this case.
 
 # %% Imports and naming conventions
 import os
+import re
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -23,7 +24,7 @@ from functions import (
 
 
 naming_map = {
-    "dw_mip_gap": "DW-MIP Gap (%)",
+    "dw_gap": "Optimality gap (%)",
     "mip_objval": "MIP Obj.val ($)",
     "dw_mip_objval": "DW-MIP Obj.val ($)",
     "dw_mip_time": "DW-MIP opt.time (s)",
@@ -45,55 +46,36 @@ def plot_scatter(df, model_name: str, xname: str, yname: str):
 
 
 # %% Load the data
-# Fast DW only contains ones with rmpgap = 10. We relax the incremental improvement to get faster solution.
-folder_name = os.path.join(get_temp_dir(), "new_dw_stats")
-filesets = os.listdir(folder_name)
+folder_name = os.path.join(get_temp_dir(), "dw_stats")
+stats = pd.read_csv(os.path.join(folder_name, "dw_experiment_outputs.csv"))
 
-# We will compare the quicker termination criteria with the slower one
-slow_dw_stats = pd.read_csv(
-    os.path.join(get_temp_dir(), "dw_stats_wo_incre_improve.csv")
-)
-slow_dw_stats["dw_gap"] = calc_percent_change(
-    slow_dw_stats["dw_mip_objval"], slow_dw_stats["true_objval"]
-)
+stats["dw_gen_col_time"] = stats["master_time"] + stats["subp_time"]
 
-slowest_dw_stats = slow_dw_stats[slow_dw_stats["set_rmpgap"] == 0.0001].reset_index(
-    drop=True
-)
+# Capitalize country names
+stats["model_name"] = stats["model_name"].str.capitalize()
 
-
-fast_dw_stats = pd.DataFrame()
-for file in filesets:
-    df = pd.read_csv(os.path.join(folder_name, file))
-    fast_dw_stats = pd.concat([fast_dw_stats, df], axis=0)
-del df
-fast_dw_stats["dw_gap"] = calc_percent_change(
-    fast_dw_stats["dw_mip_objval"], fast_dw_stats["true_objval"]
-)
-
-stats = pd.concat([fast_dw_stats, slow_dw_stats], axis=0)
-
-# set_rmpgap is in fraction and not in percentage
-stats["dw_gap"] = calc_percent_change(stats["dw_mip_objval"], stats["true_objval"])
-stats["log10_dw_total_time"] = np.log10(stats["dw_total_time"])
-
-
-# %% Plot the solution quality as boxplots
+# %% Plot the sample space of solution quality
+print("\nSample space of DW OPTGap")
 g = sns.FacetGrid(stats, row="set_rmpgap", col="set_dwimprove", margin_titles=True)
 g.map(
     sns.barplot,
     "model_name",
     "dw_gap",
+    order=["Laos", "Cambodia", "Thailand"],
     linewidth=2.5,
 )
 
+
 # %% Compare Opt.time
+print("\nSample space of DW opt.time")
 g = sns.FacetGrid(stats, row="set_rmpgap", col="set_dwimprove", margin_titles=True)
 g.map(
     sns.boxplot,
     "model_name",
     "log10_dw_total_time",
+    order=["Laos", "Cambodia", "Thailand"],
 )
+
 
 # %% Print stats
 stats_grouped = stats.groupby(["model_name", "set_rmpgap", "set_dwimprove"]).mean()
@@ -106,49 +88,91 @@ print(stats_grouped["dw_gap"])
 
 
 # %% Plot time series of the best case of DW against MIP Gurobi
-thailand_true = pd.read_csv(
-    os.path.join(get_temp_dir(), "true_values", "thailand_24.csv"), header=0
-)
-
-thailand_dw = stats[
-    (stats["model_name"] == "thailand")
-    & (stats["set_rmpgap"] == 10)
-    & (stats["set_dwimprove"] == 10)
+best_dw_thailand = stats[
+    (stats["model_name"] == "Thailand")
+    & (stats["set_rmpgap"] == 50)
+    & (stats["set_dwimprove"] == 50)
 ]
+best_dw_thailand = best_dw_thailand.reset_index(drop=True)
 
-fig, ax = plt.subplots()
-ax.plot(thailand_true["mip_opt_time"], label="MIP", alpha=0.5, linewidth=1)
-ax.plot(thailand_dw["master_time"], label="DW Master", alpha=0.5, linewidth=1)
-ax.plot(thailand_dw["subp_time"], label="DW Subproblems", alpha=0.5, linewidth=1)
-ax.plot(
-    thailand_dw["master_mip_time"], label="DW Discretization", alpha=0.5, linewidth=1
+# Something weird happen at the 108th day with 100 and 100
+
+
+def plot_log_time_series(df):
+    fig, ax = plt.subplots()
+    ax.semilogy(
+        df["mip_gurobi_time"],
+        label="MIP",
+        alpha=1,
+        linewidth=2,
+    )
+    ax.semilogy(
+        df["dw_gen_col_time"],
+        label="DW: Generating schedules",
+        alpha=0.5,
+        linewidth=1,
+    )
+    ax.semilogy(
+        df["master_mip_time"],
+        label="DW: Selecting schedules",
+        alpha=1,
+        linewidth=2,
+    )
+    ax.set_xlabel("Day")
+    ax.set_ylabel("Optimization time (s)")
+    ax.set_title("Thailand over 24-hr horizon")
+    plt.legend(bbox_to_anchor=(1.05, 1), loc="upper left", borderaxespad=0.0)
+    plt.show()
+
+
+plot_log_time_series(best_dw_thailand)
+
+
+# %% Show the effects of parameterization for dw_improve 50% and dw_rmpgap at 50%
+sel_rmpgap = 50
+param_subset = stats[
+    (stats["set_rmpgap"] == sel_rmpgap) & (stats["set_dwimprove"] >= 50)
+]
+g = sns.FacetGrid(
+    param_subset, row="set_rmpgap", col="set_dwimprove", margin_titles=True
 )
-ax.set_xlabel("Day")
-ax.set_ylabel("Opt.time (s)")
-ax.set_title("Thailand")
-plt.legend()
-plt.show()
-
-# %%
-fig, ax = plt.subplots()
-ax.semilogy(thailand_true["mip_opt_time"], label="MIP", alpha=0.5, linewidth=1)
-ax.semilogy(thailand_dw["master_time"], label="DW Master", alpha=0.5, linewidth=1)
-ax.semilogy(thailand_dw["subp_time"], label="DW Subproblems", alpha=0.5, linewidth=1)
-ax.semilogy(
-    thailand_dw["master_mip_time"], label="DW Discretization", alpha=0.5, linewidth=1
+g.map(
+    sns.boxplot,
+    "model_name",
+    "dw_gap",
+    order=["Laos", "Cambodia", "Thailand"],
+    linewidth=2.5,
 )
-ax.set_xlabel("Day")
-ax.set_ylabel("Log10 Opt.time (s)")
-ax.set_title("Thailand")
-plt.legend()
-plt.show()
+re_pat = r"""\=\s(\d+.\d+)"""
+for ax in g.axes.flatten():
+    # Get the numerical value from the title
+    dw_improve = ax.get_title()
+    dw_improve = re.findall(re_pat, dw_improve)[0]
+    ax.set_title(f"Increm. improve: {dw_improve} %")
 
-# %% Plot the speed-comparison of the fast DW
-fast_dw_stats["xSpeedup"] = (
-    fast_dw_stats["mip_gurobi_time"] / fast_dw_stats["dw_total_time"]
+    plt.setp(ax.texts, text=f"RMPGap {sel_rmpgap} (%)")
+
+    ax.set_xlabel("")
+    ax.set_ylabel("Optimality gap (%)")
+
+
+# %% Find the solution quality based on set_rmpgap = 10 and set_dwimprove = 10 for all countries
+# Need to update this plot once we have gotten additional data points
+# param_subset = param_subset[param_subset["set_dwimprove"] == 10]
+cols2group = ["model_name", "T_simulate", "set_rmpgap", "set_dwimprove"]
+param_subset.groupby(cols2group).mean()["dw_gap"]
+print(param_subset.groupby(cols2group).mean()["dw_gap"])
+
+
+# %% Plot the speed-up
+param_subset.loc[:, "xSpeedup"] = (
+    param_subset["mip_gurobi_time"] / param_subset["dw_total_time"]
 )
 g = sns.FacetGrid(
-    fast_dw_stats, row="set_rmpgap", col="set_dwimprove", margin_titles=True
+    param_subset,
+    row="set_rmpgap",
+    col="set_dwimprove",
+    margin_titles=True,
 )
 g.map(
     sns.barplot,
@@ -156,50 +180,69 @@ g.map(
     "xSpeedup",
     linewidth=2.5,
     label="xSpeedup",
+    order=["Laos", "Cambodia", "Thailand"],
 )
 for ax in g.axes.flatten():
     ax.axhline(1, ls="--", color="k", label=["1.0x"])
+    ax.set_xlabel("")
+    ax.set_ylabel("x(Optimization time)")
 
-# %%
-thailand_dw["xSpeedup"] = thailand_dw["mip_gurobi_time"] / thailand_dw["dw_total_time"]
+    # Get the numerical value from the title
+    dw_improve = ax.get_title()
+    dw_improve = re.findall(re_pat, dw_improve)[0]
+    ax.set_title(f"Increm. improve: {dw_improve} %")
+
+    plt.setp(ax.texts, text=f"RMPGap {sel_rmpgap} (%)")
+
+
+# %% Plot the time comparison for laos
+laos100 = pd.read_csv(
+    os.path.join(
+        get_temp_dir(), "dw_stats", "laos_100re_24_0.0001_0.0001_False_dwstats.csv"
+    )
+)
+laos100["dw_gen_col_time"] = laos100["master_time"] + laos100["subp_time"]
+
+# Indicate if the master_time is quicker than mip_gurobi_time
+laos100["is_master_quicker"] = laos100["mip_gurobi_time"] > laos100["master_time"]
+
 
 fig, ax = plt.subplots()
-ax.plot(thailand_dw["xSpeedup"], linewidth=1, label=["Speed-up"])
-ax.set_xlabel("Day")
-ax.set_ylabel("xSpeedup")
-ax.set_title("Thailand | DW speed-up")
-ax.axhline(1, ls="--", color="k", label=["1.0x"])
-ax.set_ylim(0, 7)
-
-# Bar plot of Gap on the secondary axis
-ax2 = ax.twinx()
-ax2.bar(
-    thailand_dw.index, thailand_dw["dw_gap"], alpha=0.25, color="r", label="DW Gap(%)"
+ax.semilogy(
+    laos100["mip_gurobi_time"],
+    label="MIP",
+    alpha=1,
+    linewidth=2,
 )
-ax2.set_ylabel("DW Gap(%)")
-ax2.set_ylim(0, 100)
+ax.semilogy(
+    laos100["dw_gen_col_time"],
+    label="DW: Generating schedules",
+    alpha=0.5,
+    linewidth=0.5,
+)
+ax.semilogy(
+    laos100["master_mip_time"],
+    label="DW: Selecting schedules",
+    alpha=1,
+    linewidth=2,
+)
 
-fig.legend()
+ax.set_xlabel("Day")
+ax.set_ylabel("Optimization time (s)")
+ax.set_title("Laos (without spinning reserve)\n over 24-hr horizon")
+
+# Highlight that area when the master_time is quicker than gurobi_mip_time
+for idx, row in laos100.iterrows():
+    if row["is_master_quicker"]:
+        ax.axvspan(
+            idx - 0.5,
+            idx + 0.5,
+            alpha=0.5,
+            color="yellow",
+            label="",
+        )
+plt.legend(bbox_to_anchor=(1.05, 1), loc="upper left", borderaxespad=0.0)
 plt.show()
 
-
-# %%
-
-sel_country = "thailand"
-temp_df = stats[stats["model_name"] == sel_country]
-
-# Drop the columns that are not needed
-cols2drop = [
-    "mip_gurobi_time",
-    "mip_objval",
-    "wall_clock_mip_gurobi",
-    "lp_gurobi_time",
-    "lp_objval",
-    "wall_clock_lp_gurobi",
-]
-
-temp_df = temp_df.drop(columns=cols2drop)
-
-temp_df.to_csv(os.path.join(get_temp_dir(), f"{sel_country}_dw_stats.csv"), index=False)
 
 # %%
