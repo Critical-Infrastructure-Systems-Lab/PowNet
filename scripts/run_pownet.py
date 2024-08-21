4f8720292630b0850a11b8551d49539c8f186c35
phumthep <phumthep@gmail.com>
2024-08-21 11:38:05 -0400

    Update run_pownet.py

--- scripts/run_pownet.py
+++ scripts/run_pownet.py
@@ -7,15 +7,22 @@ from pownet.core import (
     Visualizer,
 )
 from pownet.modeling import PowerSystemModel
+from pownet.data_processor import DataProcessor
 from pownet.data_utils import create_init_condition
 
-import gurobipy as gp
-import pandas as pd
-
 ##### User inputs #####
+to_process_inputs = True
 sim_horizon = 24
-model_name = "dummy_trade"
+model_name = "thailand"
+steps_to_run = None  # Default is None
+do_plot = True
 #######################
+
+############### Region A ################
+
+if to_process_inputs:
+    data_processor = DataProcessor(model_name=model_name, year=2016, frequency=50)
+    data_processor.execute_data_pipeline()
 
 inputs = SystemInput(
     model_name=model_name,
@@ -25,15 +32,21 @@ inputs = SystemInput(
     load_shortfall_penalty_factor=1000,
     spin_shortfall_penalty_factor=1000,
 )
+inputs.load_and_check_data()
 
-inputs.load_check_and_print_summary()
+
 model_builder = ModelBuilder(inputs)
 init_conditions = create_init_condition(inputs.thermal_units)
 
 record = SystemRecord(inputs)
 
-steps_to_run = 2  # 365 - (sim_horizon // 24 - 1)
 build_times = []
+opt_times = []
+objvals = []
+
+if steps_to_run is None:
+    steps_to_run = 10  # 365 - (sim_horizon // 24 - 1)
+
 for step_k in range(1, steps_to_run):
     start_time = datetime.now()
     if step_k == 1:
@@ -42,35 +55,69 @@ for step_k in range(1, steps_to_run):
             init_conds=init_conditions,
         )
     else:
-        # model = model_builder.update(step_k=step_k, init_conds=init_conditions)
-        model = model_builder.build(
+        model = model_builder.update(
             step_k=step_k,
             init_conds=init_conditions,
         )
-    build_time = datetime.now() - start_time
+        # model = model_builder.build(
+        #     step_k=step_k,
+        #     init_conds=init_conditions,
+        # )
+    build_times.append((datetime.now() - start_time).total_seconds())
 
     power_system_model = PowerSystemModel(model)
-    power_system_model.optimize()
+    power_system_model.optimize(mipgap=0.001)
+    objvals.append(power_system_model.get_objval())
+    opt_times.append(power_system_model.get_runtime())
 
     record.keep(power_system_model, step_k)
     init_conditions = record.get_init_conds()
-    build_times.append(build_time.total_seconds())
+
+print(f"Objvals: {' '.join([str(int(objval)) for objval in record.get_objvals()])}")
+
 
 # Process the results
 output_processor = OutputProcessor(inputs)
 node_var_df = record.get_node_variables()
-output_processor.load(node_var_df)
+output_processor.load_from_dataframe(node_var_df)
 
 # Visualize the results
-visualizer = Visualizer(inputs.model_id)
-visualizer.plot_fuelmix_bar(
-    dispatch=output_processor.get_hourly_dispatch(),
-    demand=output_processor.get_hourly_demand(),
-    to_save=False,
-)
+if do_plot:
+    visualizer = Visualizer(inputs.model_id)
+    if steps_to_run <= 3:
+        visualizer.plot_fuelmix_bar(
+            dispatch=output_processor.get_hourly_dispatch(),
+            demand=output_processor.get_hourly_demand(),
+            to_save=False,
+        )
+    else:
+        visualizer = Visualizer(inputs.model_id)
+        visualizer.plot_fuelmix_area(
+            dispatch=output_processor.get_daily_dispatch(),
+            demand=output_processor.get_daily_demand(),
+            to_save=False,
+        )
 
-# ==============================================================================
+
 # record.write_simulation_results()
 
-# # Save build_times to a csv file
-# pd.DataFrame(build_times).to_csv("build_times.csv", index=False)
+# Save build_times and objvals
+import os
+from pownet.folder_utils import get_output_dir
+import pandas as pd
+
+folder_dir = get_output_dir()
+prefix_name = "warmstart"
+df = pd.DataFrame(
+    {
+        "build_time": build_times,
+        "opt_time": opt_times,
+        "objval": objvals,
+    }
+)
+df.to_csv(
+    os.path.join(
+        folder_dir, f"{prefix_name}_{inputs.model_name}_{sim_horizon}_modelstats.csv"
+    ),
+    index=False,
+)
