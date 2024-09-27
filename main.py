d55fb88fa2f89bcc5dff117fc98145b41ff3b641
phumthep <phumthep@gmail.com>
2024-09-27 13:58:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,154 +1,113 @@
-import sys
-
-sys.path.append(
-    "/Users/dardiry/Academia/Cornell/Research/Integrated_Modeling/Model_Couplers/Analysis/PowNet/Software_Package/Version_3_Hisham/PowNet_StepByStep_GitHub_V3.2/src"
-)
-
 from datetime import datetime
-import os
-
-from pownet.simulation import Simulator
-from pownet.core import OutputProcessor, Visualizer
-from pownet.folder_utils import get_output_dir, delete_all_gurobi_solutions
+from pownet.core import (
+    SystemInput,
+    ModelBuilder,
+    SystemRecord,
+    OutputProcessor,
+    Visualizer,
+)
+from pownet.modeling import PowerSystemModel
+from pownet.data_processor import DataProcessor
+from pownet.data_utils import create_init_condition
 
 
 def main():
-    # ------- User defined inputs
-    MODEL_NAME = "dummy_trade"
-    # The default simulation horizon T is 24 hours
+    ##### User inputs #####
+    to_process_inputs = True
+    model_year = 2016
     sim_horizon = 24
-    # One year has 8760 hours. If T = 24, then we have 365 steps.
-    # STEPS = math.floor(8760/T)
-    STEPS = 3
+    model_name = "RegionAB"
+    steps_to_run = 3  # If None, then run the whole year
+    do_plot = True
+    do_write_results = True
 
-    # Decide whether to save results
-    SAVE_RESULT = config.get_saveresults()
-    SAVE_PLOT = config.get_saveplots()
-    to_reoperate = config.get_resreop()
+    ####################### Processing inputs
+    if steps_to_run is None:
+        steps_to_run = 365 - (sim_horizon // 24 - 1)
 
-    output_dir = get_output_dir()
+    # To create files with "pownet_" prefix
+    if to_process_inputs:
+        data_processor = DataProcessor(
+            model_name=model_name, year=model_year, frequency=50
+        )
+        data_processor.execute_data_pipeline()
 
-    # We need a folder to store the figures
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    inputs = SystemInput(
+        model_name=model_name,
+        year=model_year,
+        sim_horizon=sim_horizon,
+        spin_reserve_factor=0.15,
+        load_shortfall_penalty_factor=1000,
+        load_curtail_penalty_factor=1,
+        spin_shortfall_penalty_factor=1000,
+    )
+    # Produce an error if the data is not making sense
+    inputs.load_and_check_data()
 
-    if STEP_BY_STEP:  # STEP_BY_STEP Version
-        print("\n\n Running STEP_BY_STEP Version..........")
-        for SIM_DAY in range(1, STEPS + 1):
-            run_pownet(
-                MODEL_NAME,
-                T,
-                STEPS,
-                SIM_DAY,
-                use_gurobi,
-                to_reoperate,
-                output_dir,
-                SAVE_RESULT,
-                SAVE_PLOT,
+    ####################### Simulation
+    record = SystemRecord(inputs)
+    model_builder = ModelBuilder(inputs)
+
+    # Initially, all thermal units are off. They have to be switched on from cold start
+    init_conditions = create_init_condition(inputs.thermal_units)
+
+    for step_k in range(1, steps_to_run):
+        if step_k == 1:
+            model = model_builder.build(
+                step_k=step_k,
+                init_conds=init_conditions,
+            )
+        else:
+            model = model_builder.update(
+                step_k=step_k,
+                init_conds=init_conditions,
             )
 
-    elif ONE_STEP:  # ONE_STEP Version
-        print("\n\n Running ONE_STEP Version..........")
-        SIM_DAY = config.get_simday()
-        run_pownet(
-            MODEL_NAME,
-            T,
-            STEPS,
-            SIM_DAY,
-            use_gurobi,
-            to_reoperate,
-            output_dir,
-            SAVE_RESULT,
-            SAVE_PLOT,
+        power_system_model = PowerSystemModel(model)
+        power_system_model.optimize(mipgap=0.001)
+
+        record.keep(
+            runtime=power_system_model.get_runtime(),
+            objval=power_system_model.get_objval(),
+            solution=power_system_model.get_solution(),
+            step_k=step_k,
         )
+        init_conditions = record.get_init_conds()
 
-    else:  # ALL_STEPS Version
-        print("\n\n Running ALL_STEPS Version..........")
-        SIM_DAY = STEPS
-        run_pownet(
-            MODEL_NAME,
-            T,
-            STEPS,
-            SIM_DAY,
-            use_gurobi,
-            to_reoperate,
-            output_dir,
-            SAVE_RESULT,
-            SAVE_PLOT,
-        )
-
-
-def run_pownet(
-    MODEL_NAME,
-    T,
-    STEPS,
-    SIM_DAY,
-    use_gurobi,
-    to_reoperate,
-    output_dir,
-    SAVE_RESULT,
-    SAVE_PLOT,
-):
-
-    time_start = datetime.now()
-
-    simulator = Simulator(
-        model_name=MODEL_NAME,
-        T=sim_horizon,
-        to_reoperate=to_reoperate,
+    ####################### Processing outputs
+    output_processor = OutputProcessor(
+        year=inputs.year,
+        fuelmap=inputs.fuelmap,
+        demand=inputs.demand,
     )
+    node_var_df = record.get_node_variables()
+    output_processor.load_from_dataframe(node_var_df)
 
-    record = simulator.run(steps=STEPS)
+    # If objvals are needed
+    objvals = record.get_objvals()
 
-    if SAVE_RESULT:
-        record.to_csv()
+    ####################### Visualize the results
+    if do_plot:
+        visualizer = Visualizer(inputs.model_id)
+        if steps_to_run <= 3:
+            visualizer.plot_fuelmix_bar(
+                dispatch=output_processor.get_hourly_dispatch(),
+                demand=output_processor.get_hourly_demand(),
+                to_save=False,
+            )
+        else:
+            visualizer = Visualizer(model_id=inputs.model_id)
+            visualizer.plot_fuelmix_area(
+                dispatch=output_processor.get_daily_dispatch(),
+                demand=output_processor.get_daily_demand(),
+                to_save=False,
+            )
 
-    print("\n\n====")
-    print(f"PowNet: Solved {MODEL_NAME}")
-    print(
-        f"PowNet: Total time (s) = {round((datetime.now() - time_start).total_seconds(), 2)}"
-    )
-    print(f"PowNet: Opt.time (s) = {round(sum(simulator_run.runtimes), 2)}")
-
-    # 5) Save PowNet Results
-    if SAVE_RESULT:
-        simulator_run.to_csv()
-
-    # Export reservoir outputs as csv
-    if to_reoperate:
-        simulator.export_reservoir_outputs()
-
-    node_variables = record.get_node_variables()
-
-    system_input = simulator.get_system_input()
-    output_processor = OutputProcessor()
-    output_processor.load(
-        df=node_variables, system_input=system_input, model_name=MODEL_NAME
-    )
-
-    visualizer = Visualizer(model_name=MODEL_NAME, ctime=output_processor.ctime)
-
-    # The dispatch plot does not work well when simulating more than 2 day or
-    # there are more than 10 thermal units.
-    if STEPS <= 48 and len(system_input.thermal_units) <= 10:
-        visualizer.plot_thermal_units(
-            thermal_dispatch=output_processor.get_dispatch(),
-            unit_status=output_processor.get_unit_status(),
-            thermal_units=system_input.thermal_units,
-            full_max_cap=system_input.full_max_cap,
-            to_save=SAVE_PLOT,
-        )
-        visualizer.plot_fuelmix_area(
-            dispatch=output_processor.get_total_dispatch(),
-            demand=output_processor.get_total_demand(),
-            to_save=SAVE_PLOT,
-        )
-    else:
-        visualizer.plot_fuelmix_area(
-            dispatch=output_processor.get_monthly_dispatch(),
-            demand=output_processor.get_monthly_demand(),
-            to_save=SAVE_PLOT,
-        )
+    ####################### Write outputs
+    # Write three csv files to the output directory: node variables,
+    # system variables, and flow variables
+    if do_write_results:
+        record.write_simulation_results()
 
 
 if __name__ == "__main__":
