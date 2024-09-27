aa2fadf7590b35d35a19b1c69539935fdf121352
Stefano Galelli <galelli@cornell.edu>
2024-09-27 13:08:27 -0400

    Delete src/pownet/reservoir directory

--- src/pownet/reservoir/__init__.py
+++
@@ -1,5 +0,0 @@
-""" This is the reservoir module.
-"""
-
-from .reservoir import Reservoir, Basin, ReservoirOperator
-from .solve_release import solve_release_from_storage, solve_release_from_dispatch
